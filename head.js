08ae395a77b107abcf690d1eafaca0a3d1c46f62
Nick <nick.pavelich@gmail.com>
2021-06-21 20:50:21 -0600

    Implement head challenge

--- head.js
+++ head.js
@@ -7,7 +7,14 @@ const assertEqual = function(actual, expected) {
 
 };
 
+const head = function(array) {
+  return array[0];
+}
+
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual(7, 2);
 assertEqual("Apple", "Apple");
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
