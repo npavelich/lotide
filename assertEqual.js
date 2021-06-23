8afde48398d4023eb0d38125cbfd914bd9ccd999
Nick <nick.pavelich@gmail.com>
2021-06-22 19:46:02 -0600

    Refactored code

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
   }
 
 };
