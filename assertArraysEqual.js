475ec417b89edbe1b97d082ec4925d27fe3074f2
Nick <nick.pavelich@gmail.com>
2021-06-22 20:01:28 -0600

    Combined functions

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false
+    }
+  }
+  return true
+}
+
+const assertArraysEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `The arrays and are the same`;
+  } else {
+    return `The arrays are not the same`;
+  }
+
+};
+
+console.log(assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true))
